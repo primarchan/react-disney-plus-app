32b045c281346d326d9fb1238d96f9a5e861e2e3
An T.G <boltcarrier@naver.com>
2023-07-31 13:41:45 +0900

    useParams 를 이용한 영화 상세 페이지 구현

--- src/pages/DetailPage/index.js
+++ src/pages/DetailPage/index.js
@@ -1,7 +1,31 @@
-import React from "react";
+import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+
+import axios from "../../api/axios";
 
 const DetailPage = () => {
-  return <div>DetailPage</div>;
+  const [movie, setMovie] = useState({});
+  let { movieId } = useParams();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(`/movie/${movieId}`);
+      console.log(response);
+      setMovie(response.data);
+    };
+    fetchData();
+  }, [movieId]);
+
+  if (!movie) return null;
+  return (
+    <section>
+      <img
+        className="modal__poster-img"
+        src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+        alt="modal__poster-img"
+      />
+    </section>
+  );
 };
 
 export default DetailPage;
